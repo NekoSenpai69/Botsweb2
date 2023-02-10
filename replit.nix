ee11ec2fafd65a283e707a41c3b65e09041c84af
Neko Senpai <123672103+NekoSenpai69@users.noreply.github.com>
2023-02-10 00:59:14 +0000

    Server

--- replit.nix
+++ replit.nix
@@ -1,8 +1,16 @@
 { pkgs }: {
+<<<<<<< HEAD
 	deps = [
 		pkgs.nodejs-18_x
     pkgs.nodePackages.typescript-language-server
     pkgs.yarn
     pkgs.replitPackages.jest
 	];
+=======
+  deps = [
+    pkgs.nodejs-16_x
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server
+  ];
+>>>>>>> origin/main
 }
