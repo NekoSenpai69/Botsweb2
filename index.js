f9fea7823ff48adb9e6c6a718d85c57cbf25dce3
Neko Senpai <123672103+NekoSenpai69@users.noreply.github.com>
2023-02-10 00:52:24 +0000

    Server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT || 8080 
+const router = express.Router();
+const path = require("path");
+const app = require('express')();
+
+app.get('/',(req,res) => {
+res.sendFile(path.join(__dirname + '/index.html'))});
+app.get('/home',(req,res) => {
+res.sendFile(path.join(__dirname + '/index.html'))});
+
+app.get('/contact',(req,res) => {
+res.sendFile(path.join(__dirname + '/contact.html'))});
+
+app.get('/about',(req,res) => {
+res.sendFile(path.join(__dirname + '/about.html'))});
+
+
+app.listen(port => {
+  console.log(`successful running on ${PORT} `)
+})
